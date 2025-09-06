e8584f4153f239b140d07848b0a469a9e484b797
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2025-09-06 13:22:56 +0530

    Fix Landing Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,51 @@
-import Wrapper from "@/components/layout/Wrapper";
-import MainHome from "../app/(homes)/home_1/page";
+// import Wrapper from "@/components/layout/Wrapper";
+// import MainHome from "../app/(homes)/landing_page/page";
+
+// export const metadata = {
+//   title: "GoKite - Travel & Tour",
+//   description: "GoKite - Travel & Tour",
+// };
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Wrapper>
+//         <MainHome />
+//       </Wrapper>
+//     </>
+//   );
+// }
+import DefaultFooter from "@/components/footer/default";
+import Header1 from "@/components/header/Navbar";
+import Hero1 from "@/components/landingpage/hero";
+import HolidayDestinations from "@/components/landingpage/holiday-destintion";
+import VisaDestinationCards from "@/components/landingpage/visa-destination";
+import HotelDealsCards from "@/components/landingpage/Hotel-deals";
+import Hotels from "@/components/landingpage/Hotels";
 
 export const metadata = {
-  title: "Home-1 || GoKite - Travel & Tour",
-  description: "GoKite - Travel & Tour",
+  title: "GoKite - Travel & Tour ",
+  description: "GoKite - Travel & Tour ",
 };
 
-export default function Home() {
+const Home_1 = () => {
   return (
     <>
-      <Wrapper>
-        <MainHome />
-      </Wrapper>
+      <Header1 />
+
+      <Hero1 />
+
+      <HolidayDestinations />
+
+      <VisaDestinationCards />
+
+      <Hotels />
+
+      <HotelDealsCards />
+
+      <DefaultFooter />
     </>
   );
-}
+};
+
+export default Home_1;
