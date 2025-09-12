a8fb49233ec57bafa26662f038123a420fe5e2d5
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2025-09-12 13:53:59 +0530

    Update: Footer, Visa Page Linkage

--- components/landingpage/hero/index.jsx
+++ components/landingpage/hero/index.jsx
@@ -240,7 +240,7 @@ const IconRow = () => {
       redirectUrl: '/holidays'
     },
     { id: "Hotel", label: "Hotel", imgSrc: "/img/landingpage/icons/hotel.png", redirectUrl: '#' },
-    { id: "Visa", label: "Visa", imgSrc: "/img/landingpage/icons/visa.png", redirectUrl: '/master_visa' },
+    { id: "Visa", label: "Visa", imgSrc: "/img/landingpage/icons/visa.png", redirectUrl: '/landing_page_visa_home' },
     { id: "More", label: "More", imgSrc: "/img/landingpage/icons/more.png", redirectUrl: '#' },
   ];
   return (
