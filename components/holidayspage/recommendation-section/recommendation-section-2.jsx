d774530b008b13e28c766877528b79486c57c255
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2025-09-15 14:58:42 +0530

    Fix: Responsive holidays page

--- components/holidayspage/recommendation-section/recommendation-section-2.jsx
+++ components/holidayspage/recommendation-section/recommendation-section-2.jsx
@@ -1,12 +1,15 @@
 const { default: RecommendationDestinations2 } = require("./recommendation-dest-2")
 const { default: RecommendationDestinations3 } = require("./recommendation-dest-3")
-
+import './recommendation-section-2.styles.css'
 const RecommendationSection2 = () => {
 
     return (
         <>
-            <RecommendationDestinations2 />
-            <RecommendationDestinations3 />
+            <div className="recom-sec-2">
+                <RecommendationDestinations2 />
+                <RecommendationDestinations3 />
+            </div>
+
         </>
 
     )
