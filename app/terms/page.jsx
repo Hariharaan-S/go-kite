00c41e6344e5d2803bb2863e9cee3c641c17b4c2
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2025-10-06 12:10:26 +0530

    Code Cleaning

--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -2,7 +2,6 @@
 import Header1 from "@/components/header/Navbar";
 import GoKiteFooter from "@/components/footer/footer-gokite";
 import TermsConent from "@/components/terms/TermsConent";
-import GoKiteFooter from "@/components/footer/footer-gokite";
 
 export const metadata = {
   title: "Terms & Conditions || GoKite - Travel & Tour ",
